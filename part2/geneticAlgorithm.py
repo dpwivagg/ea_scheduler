456477134b9f0e896a3ffba3da5b04307ea6de6c
Daniel Wivagg <dpwivagg@gmail.com>
2018-02-10 15:56:47 -0500

    Fill in crossover function

--- part2/geneticAlgorithm.py
+++ part2/geneticAlgorithm.py
@@ -1,6 +1,9 @@
 #This is for Genetic Algorithm
 
 original_map = []
+""" Change these to be the correct values during runtime """
+map_width = 5
+map_height = 5
 
 class map():
     def __init__(self, map_matrix):
@@ -18,7 +21,17 @@ class map():
         pass
 
     def crossover(self, other):
-        pass
+        first_new_map = {}
+        second_new_map = {}
+        for i in range(0, map_width//2):
+            for j in range(0, map_height):
+                first_new_map[i, j] = self.map_matrix[i, j]
+                second_new_map[i, j] = other.map_matrix[i, j]
+
+        for i in range(map_width//2, map_width):
+            for j in range(0, map_height):
+                first_new_map[i, j] = other.map_matrix[i, j]
+                second_new_map[i, j] = self.map_matrix[i, j]
 
 
 def crossover(map1, map2):
