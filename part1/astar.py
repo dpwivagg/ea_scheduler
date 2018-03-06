a023ba455b8b9bf5b2a9ce3b7378af713c2828a3
alexfangying <ying@autoreg-132131.dyn.wpi.edu>
2018-03-06 15:23:34 -0500

    add a

--- part1/astar.py
+++ part1/astar.py
@@ -7,7 +7,7 @@ from copy import deepcopy
 Heapq.heappush: Add an item to the priority queue
 Heapq.heappop: Remove the smallest item from the queue
 """
-## aaa
+## aaaa
 
 class gameBoard():
     def __init__(self, positionArray, actionCost=0,parentBoard = None,heuristicOriginal = 0):
