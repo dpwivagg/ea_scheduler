df88cdd7de0d0074c146660c832592f8e23f79ea
MingquanLiu <mikelmq99@gmail.com>
2018-03-15 10:58:11 -0400

    Structures

--- Assignment2/Node.py
+++ Assignment2/Node.py
@@ -1,1 +1,11 @@
 # This is the Node for Gibbs sampling
+class Node:
+    def __init__(self, identity, cpt):
+        self.identity = identity
+        self.cpt = cpt
+        self.state = 0
+        self.parents = {}  # The parent node list
+        self.children = {} # The children node list
+        self.isEvidence = False
+
+    def
