2b09d2f3976c84d00af79e48496de499d6414340
alexfangying <ying@autoreg-132131.dyn.wpi.edu>
2018-03-16 17:14:38 -0400

    New commit

--- Assignment2/new_input.py
+++ Assignment2/new_input.py
@@ -5,39 +5,35 @@ def input_line():
     line = input()
     line_split = line.split(' ')
     evidence_node = {}
-    #print (line_split)
 
     for number in range(len(line_split)):
 
         if line_split[number] == 'gibbs':
             query_node = line_split[(number + 1)]
-            print(query_node)
 
         if line_split[number] == '-u':
             update_number = line_split[(number + 1)]
-            #print(update_number)
 
         if line_split[number] == '-d':
             drop_number = line_split[(number + 1)]
-            print(drop_number)
 
     for i, s in enumerate(line_split):
         if '=' in s:
             s_split = s.split('=')
-            # evidence_node[s_split[0]]=s_split[1]
             input_state = str(s_split[1])
-            print(input_state)
-
             if input_state in ['good', 'lots', 'small', 'old', 'cheap']:
                 state = 0
-
             elif input_state in ['bad', 'little', 'medium', 'new', 'ok']:
                 state = 1
-
             elif input_state in ['large', 'ugly', 'expensive']:
                 state = 2
-
             evidence_node[s_split[0]] = state
-    print(evidence_node)
+
+    print('Query node:' + '\t' + str(query_node))
+    print('Evidence nodes:' + '\t' + str(evidence_node))
+    print('Update number:' + '\t' + str(update_number))
+    print('Drop number:' + '\t' + str(drop_number))
 
     return query_node, evidence_node, update_number, drop_number
+
+input_line()
