762dd1be06fce38b254df384527f50ad433a3ebd
alexfangying <ying@autoreg-132131.dyn.wpi.edu>
2018-03-16 16:40:05 -0400

    New_input_revise

--- Assignment2/new_input.py
+++ Assignment2/new_input.py
@@ -24,9 +24,16 @@ def input_line():
     for i, s in enumerate(line_split):
         if '=' in s:
             s_split = s.split('=')
-            evidence_node[s_split[0]]=s_split[1]
+            # evidence_node[s_split[0]]=s_split[1]
+            if s_split[1] == 'good' or 'lots' or 'small' or 'old' or 'cheap':
+                state = 0
+            if s_split[1] == 'bad' or 'little' or 'medium' or 'new' or 'ok':
+                state = 1
+            if s_split[1] == 'large' or 'ugly' or 'expensive':
+                state = 2
+            evidence_node[s_split[0]] = state
 
-    #print(evidence_node)
+    print(evidence_node)
 
     return query_node, evidence_node, update_number, drop_number
 
