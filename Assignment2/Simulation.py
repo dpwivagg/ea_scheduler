08cb37d6fa3e92bd2667cdf5c333d8c8a07964cd
mli7@wpi.edu <lepetitprince_b612@163.com>
2018-03-19 14:50:05 -0400

    New commit

--- Assignment2/Simulation.py
+++ Assignment2/Simulation.py
@@ -302,63 +302,62 @@ def createTables():
 
 
     pnode = None
-    ylist = list()
-    # sweep through the node
-    for i in range(iterations):
-        SweepIndex = i % len(actualList)
-        cnode = actualList[SweepIndex]
+
+    # # sweep through the node
+    # for i in range(drops):
+    #     SweepIndex = i % len(actualList)
+    #     cnode = actualList[SweepIndex]
+    #     while cnode == pnode:
+    #         SweepIndex = i % len(actualList)
+    #         cnode = actualList[SweepIndex]
+    #         cnode.getMBProbability()
+    #         pnode = cnode
+    # ylist = list()
+    # for i in range(iterations-drops):
+    #     SweepIndex = i % len(actualList)
+    #     cnode = actualList[SweepIndex]
+    #     while cnode == pnode:
+    #         SweepIndex = i % len(actualList)
+    #         cnode = actualList[SweepIndex]
+    #     cnode.getMBProbability()
+    #     result[qnode.state] = result[qnode.state] + 1
+    #     sum = 0
+    #     for a in result:
+    #         sum = sum +a
+    #     for a in range(len(result)):
+    #         prob[a] = result[a] / sum
+    #     for a in range(len(prob)):
+    #         graphList[a].append(prob[a])
+    #     ylist.append(i)
+    #     pnode = cnode
+
+    # sample randomly
+    for i in range(drops):
+        randIndex = random.randint(0,len(actualList)-1)
+        cnode = actualList[randIndex]
         while cnode == pnode:
-            i = i + 1
-            SweepIndex = i % len(actualList)
-            cnode = actualList[SweepIndex]
+            randIndex = random.randint(0, len(actualList)-1)
+            cnode = actualList[randIndex]
+            cnode.getMBProbability()
+            pnode = cnode
+    ylist = list()
+    for i in range(iterations-drops):
+        randIndex = random.randint(0,len(actualList)-1)
+        cnode = actualList[randIndex]
+        while cnode == pnode:
+            randIndex = random.randint(0, len(actualList)-1)
+            cnode = actualList[randIndex]
         cnode.getMBProbability()
         result[qnode.state] = result[qnode.state] + 1
-        if i<drops:
-            dropResult[qnode.state] = dropResult[qnode.state] + 1
         sum = 0
         for a in result:
             sum = sum +a
         for a in range(len(result)):
             prob[a] = result[a] / sum
-
-        if i>=drops:
-            for a in range(len(prob)):
-                graphList[a].append(prob[a])
-            ylist.append(i)
+        for a in range(len(prob)):
+            graphList[a].append(prob[a])
+        ylist.append(i)
         pnode = cnode
-
-
-    # # sample randomly
-    # for i in range(iterations):
-    #     RandomIndex = random.randint(0,len(actualList)-1)
-    #     cnode = actualList[RandomIndex]
-    #     while cnode == pnode:
-    #         i = i + 1
-    #         RandomIndex = random.randint(0,len(actualList)-1)
-    #         cnode = actualList[RandomIndex]
-    #     cnode.getMBProbability()
-    #     result[qnode.state] = result[qnode.state] + 1
-    #     if i < drops:
-    #         dropResult[qnode.state] = dropResult[qnode.state] + 1
-    #     sum = 0
-    #     for a in result:
-    #         sum = sum + a
-    #     for a in range(len(result)):
-    #         prob[a] = result[a] / sum
-    #
-    #     if i >= drops:
-    #         for a in range(len(prob)):
-    #             graphList[a].append(prob[a])
-    #         ylist.append(i)
-    #     pnode = cnode
-    #
-    for i in range(len(result)):
-        result[i] = result[i] - dropResult[i]
-    sum = 0
-    for a in result:
-        sum = sum + a
-    for a in range(len(result)):
-        prob[a] = result[a] / sum
 
 
 
@@ -391,9 +390,10 @@ def createTables():
     plt.xlabel('Iterations')
     # naming the y axis
     plt.ylabel('Probability')
+    plt.ylim(0,1)
     # giving a title to my graph
 
-    plt.title('randomly sample')
+    plt.title('gibbs schools amenities=little size=small location=ugly -u 50000 -d 0')
 
     # show a legend on the plot
     plt.legend()
