490647cb68b1e326342e67639bf4ac95cedd5a56
alexfangying <ying@autoreg-132131.dyn.wpi.edu>
2018-03-26 20:40:25 -0400

    add input

---
+++ Assignment3/input.py
@@ -0,0 +1,16 @@
+# input line should be like "em data.txt 3"
+
+def input_line():
+
+    line = input()
+    line_split = line.split(" ")
+    file_name = str
+    cluster_number = int
+
+    for i in range(len(line_split)):
+        if i == 1:
+            file_name = line_split[1]
+        if i == 2:
+            cluster_number = line_split[2]
+
+    return file_name, cluster_number
--- Assignment3/test.py
+++ Assignment3/test.py
@@ -1,6 +1,8 @@
+# test input: em sample_EM_data_v2.csv 3
 from basic_em_class import basic_em_class
+from input import input_line
 
-a = basic_em_class('sample_EM_data_v2.csv')
+a = basic_em_class(input_line()[0])
 vars = a.run()
 
 print("First distribution: \n")
