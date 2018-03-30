b2a88760a83018f4802e5b462184fa04a476ed6d
alexfangying <33532935+alexfangying@users.noreply.github.com>
2018-03-30 17:52:36 -0400

    Update test.py

--- Assignment3/test.py
+++ Assignment3/test.py
@@ -12,15 +12,16 @@ if input[1] == "X":
     clusterNum_list = range(1, 20)
     for i in clusterNum_list:
         a = basic_em_class(input[0], i)
-        vars = a.run()
-        bic_list = np.append(bic_list, vars)
-
-    number = int(bic_list.argmax()) + 2
+        output = a.run()
+        print("BIC value is: " + str(output[0]))
+        bic_list = np.append(bic_list, output[0])
+    number = int(bic_list.argmin()) + 1
     print("Best cluster number: " + str(number))
-
     plt.plot(clusterNum_list, bic_list)
     plt.xticks(np.arange(min(clusterNum_list), max(clusterNum_list) + 1, 1.0))
+    plt.xlabel("Cluster Number")
     plt.ylabel("BIC Value")
+    plt.title("Model Performance with Different Cluster Numbers")
     plt.show()
 else:
     a = basic_em_class(input[0], int(input[1]))
