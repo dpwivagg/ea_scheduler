e09bee8f3bdd79c349a4ac9ff6a44d51ffdc4908
mli7@wpi.edu <lepetitprince_b612@163.com>
2018-04-07 12:34:48 -0400

    New commit

--- Assignment4/RandomStart.py
+++ Assignment4/RandomStart.py
@@ -1,26 +1,27 @@
 import random
 def RandomStart():
-    game_board={}
+
+    board={}
 
     for i in range (0,6):
         for j in range (0,7):
-            game_board[i,j]= 0.0
+            board[i,j]= 0.0
 
-    game_board[2, 2] = "p"
-    game_board[2, 3] = "p"
-    game_board[3, 1] = "p"
-    game_board[3, 5] = "p"
-    game_board[4, 2] = "p"
-    game_board[4, 3] = "p"
-    game_board[4, 4] = "p"
-    game_board[3, 2] = "p"
+    board[2, 2] = "p"
+    board[2, 3] = "p"
+    board[3, 1] = "p"
+    board[3, 5] = "p"
+    board[4, 2] = "p"
+    board[4, 3] = "p"
+    board[4, 4] = "p"
+    board[3, 2] = "p"
 
-    row = random.randint(1, 5)
-    column = random.randint(1,6)
+    row = random.randint(0, 5)
+    column = random.randint(0,6)
 
-    while game_board[row,column] == "p":
-        row =random.randint(1,5)
-        column = random.randint(1, 6)
+    while board[row,column] == "p":
+        row =random.randint(0,5)
+        column = random.randint(0, 6)
 
     return row, column
 
