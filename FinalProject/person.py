2047e3dcf36b8f5e5436d4ce118751c47346062e
Daniel Wivagg <dpwivagg@wpi.edu>
2018-04-21 11:40:02 -0400

    Update person class to use event ID only

--- FinalProject/person.py
+++ FinalProject/person.py
@@ -1,17 +1,17 @@
 from functools import reduce
 
 class Person():
-    def __init__(self, id, roles, events, availabilities):
+    def __init__(self, id, roles, eventIDs, availabilities):
         self.id = id # Integer
         self.roles = roles # List of strings
-        self.events = events # List of objects (events)
+        self.eventIDs = eventIDs # List of Integers (event IDs)
         self.availabilities = availabilities # 2D array (events x times)
 
     def calc_heuristic(self):
         h = 0
 
         # Calculate the time this person spent working
-        time = reduce(lambda x, y: x + len(self.availabilities[x.id]), self.events)
+        time = reduce(lambda x, y: x + len(self.availabilities[y]), self.eventIDs)
         if time < 6:
             # Time is less than 6 half hours
             h -= 10
@@ -22,7 +22,7 @@ class Person():
             h += 5
 
         # Calculate the number of roles
-        h += 5 if len(self.roles) > len(self.events)/3 else -5
+        h += 5 if len(self.roles) > len(self.eventIDs)/3 else -5
 
         # Calculate number of lead/presenter roles
         h += 5 if self.roles.count("PRESENTER") <= 2 else -5
