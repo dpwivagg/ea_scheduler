7f36eabcbc537c9b12a46508395604ebb6d3aa32
mli7@wpi.edu <lepetitprince_b612@163.com>
2018-04-25 16:37:31 -0400

    new commit

--- FinalProject/hillclimbing.py
+++ FinalProject/hillclimbing.py
@@ -1,6 +1,7 @@
 # TODO: implement hill climbing algorithm
 import random
 from FinalProject.person import Person
+from FinalProject.schedule import Schedule
 from FinalProject.data_parser import read_data
 
 def hill_climbing():
@@ -20,13 +21,13 @@ def hill_climbing():
     if len(next_move) > 0:
         x = random.randint(0, len(next_move) - 1)
         best_move = next_move[x]
-
     return best_move, fitness
 
 role_list=["PRESENTER","INTRO","LEAD","DEBRIEF"]
 
 def start():
     allpeople = read_data()
+    sch= Schedule([],{},[])
     for person in allpeople:
         avaeventlist = []
         for i in range(0,21):
@@ -35,17 +36,33 @@ def start():
         # print(avaeventlist)
         x = random.randint(1,len(avaeventlist))
         person.eventIDs = random.sample(avaeventlist, x)
+        person.eventIDs.sort()
 
         for j in range(len(person.eventIDs)):
             x = random.randint(0,3)
             person.roles.append(role_list[x])
 
     # for person in allpeople:
-    #     print(person.roles, person.eventIDs)
+    #     print(person.eventIDs)
+
+    for i in range(1,22):
+        for j in range(1,7):
+            sch.events_avaibilities[i,j]=[]
+
+    for person in allpeople:
+        for i in range(len(person.eventIDs)):
+            event = person.eventIDs[i]
+            for j in range(len(person.availabilities[event-1])):
+                time = person.availabilities[event-1][j]
+                sch.events_avaibilities[event,time].append(person.id)
+                if person.id not in sch.persons:
+                    sch.persons.append(person.id)
+
+    # print(sch.persons, sch.events_avaibilities)
     return allpeople
 
 
-
+a = start()
 
 
 
