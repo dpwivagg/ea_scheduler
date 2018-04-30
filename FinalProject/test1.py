7dcb7b95cc8b7abac9181e323badd284bac39580
alexfangying <ying@Yings-MacBook-Pro.local>
2018-04-30 19:41:09 -0400

    New

--- FinalProject/test1.py
+++ FinalProject/test1.py
@@ -36,13 +36,28 @@ from FinalProject.event import Event
 from FinalProject.data_parser import read_data
 from datetime import datetime
 
+allpeople = read_data()
+event = {}
 
+for PID in allpeople.keys():
+    ava = allpeople[PID].availabilities
+    for i in range(21):
+        if ava[i] != 0:
+            if i not in event.keys():
+                event[i] = []
+                event[i].append(ava[i])
+            else:
+                event[i].append(ava[i])
+for item in event.keys():
+    print(str(event[item]) + '\n')
 
 def start():
-    role_list=["PRESENTER","INTRO","LEAD","DEBRIEF"]
+    role_list=["PRESENTER","INTRO","LEAD","DEBRIEF","NO_ROLE"]
     allpeople = read_data()
     allevents = {}
-    for i in range(0, 21):
+    # for i in allpeople.keys():
+    #     print(str(allpeople[i].availabilities))
+    for i in range(1, 22):
         event = Event(i, {})
         allevents[i] = event
     for personID in allpeople.keys():
@@ -51,22 +66,24 @@ def start():
             if allpeople[personID].availabilities[i][0] != 0:
                 avaeventlist.append(i+1)
         # print(avaeventlist)
-        x = random.randint(1,len(avaeventlist))
+        x = random.randint(0,len(avaeventlist))
         allpeople[personID].eventIDs = random.sample(avaeventlist, x)
         allpeople[personID].eventIDs.sort()
 
         for j in range(len(allpeople[personID].eventIDs)):
-            x = random.randint(0,3)
+            x = random.randint(0,4)
             allpeople[personID].roles[role_list[x]] += 1
-            allevents[j].roles_filled[role_list[x]].append(personID)
-            allevents[j].id = j+1
-            # print(allevents[j].roles_filled)
+            allevents[j+1].roles_filled[role_list[x]].append(personID)
+            allevents[j+1].id = j+1
+            # print("Event " + str(j) + str(allevents[j].roles_filled))
 
         # print(personID)
         # print(allpeople[personID].roles)
         # print(allpeople[personID].eventIDs)
-    # for i in range(21):
-    #     print(allevents[i].id)
-        # print(allevents[i].roles_filled)
 
-start()
+    # for i in range(1, 22):
+    #     print(allevents[i].id)
+    #     print(allevents[i].roles_filled)
+    return allpeople, allevents
+
+# start()
